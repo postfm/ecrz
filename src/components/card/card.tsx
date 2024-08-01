7e9ca23eb12700de58d494d6f24139b930e00cec
postfm <72159645+postfm@users.noreply.github.com>
2024-08-01 20:34:49 +0300

    feat: Добавит стили и изображения для карточек

--- src/components/card/card.tsx
+++ src/components/card/card.tsx
@@ -1,19 +1,33 @@
+import Image from 'next/image';
+import Photo1 from '../../../public/images/photo-1.png';
+
 export default function Card() {
   return (
-    <div className='flex flex-col w-[308px] h-[440px] rounded-xl bg-light-blue-100'>
-      <div className='w-full h-[214px] bg-blue-gray-300'></div>
-      <div className='flex flex-col w-full h-auto bg-red-100 p-[18px] pb-6'>
+    <div className='flex flex-col w-[308px] h-[440px] rounded-xl bg-white'>
+      <div className='w-full h-[214px]'>
+        <Image
+          src={Photo1}
+          alt=''
+        />
+      </div>
+      <div className='flex flex-col w-full h-auto p-[18px] pb-6'>
         <div className='flex flex-col items-start pb-4 border-b-2'>
-          <div>Квартира в новостройке</div>
-          <div>2-ком. квартира, 56м2</div>
-          <div>Брест, ул. Карла Маркса, 12</div>
+          <div className='font-medium text-xs text-gray-600'>Квартира в новостройке</div>
+          <div className='font-medium text-lg text-blue-600'>
+            2-ком. квартира, 56м<sup>2</sup>
+          </div>
+          <div className='font-normal text-sm text-gray-800'>Брест, ул. Карла Маркса, 12</div>
         </div>
         <div className='flex flex-col items-start'>
-          <div>14м2 25м2 3/10 этаж</div>
-          <div className='flex'>
-            <div>$ 60 000</div>
+          <div className='mt-4 mb-4 font-normal text-sm text-gray-800'>
+            14м<sup>2</sup> 25м<sup>2</sup> 3/10 этаж
+          </div>
+          <div className='flex font-medium text-lg mb-2'>
+            <div className=' pr-4 border-r-2 mr-4'>$ 60 000</div>
             <div>BYN 197 797</div>
-            <div>$ 1000/м2</div>
+          </div>
+          <div className='font-normal text-sm text-gray-600'>
+            $ 1000 / м<sup>2</sup>
           </div>
         </div>
       </div>
