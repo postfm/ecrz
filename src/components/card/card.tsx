9829f889987ffb0388b1cf9e215b14ac66b89931
postfm <postfm@yandex.ru>
2024-08-18 22:52:02 +0100

    fix: поправит цвета кнопок

--- src/components/card/card.tsx
+++ src/components/card/card.tsx
@@ -25,7 +25,9 @@ export default function Card({ property, rentalType }: CardProps) {
                 <span
                   key={i}
                   className={`block h-1 cursor-pointer rounded-2xl transition-all content-[''] ${
-                    activeIndex === i ? 'w-[28px] h-1 bg-blue-600' : 'w-[28px] h-1 bg-white/50'
+                    activeIndex === i
+                      ? 'w-[28px] h-1 bg-blue-600'
+                      : 'w-[28px] h-1 bg-white/50'
                   }`}
                   onClick={() => setActiveIndex(i)}
                 />
@@ -102,31 +104,29 @@ export default function Card({ property, rentalType }: CardProps) {
       <div className='flex flex-col w-full h-auto p-[18px] pb-6'>
         <div className='flex flex-col items-start pb-4 border-b-2'>
           {rentalType === RentalType.Apartment && (
-            <div className='font-medium text-xs text-gray-600'>{property.roomType}</div>
+            <div className='font-medium text-xs text-gray-600'>
+              {property.roomType}
+            </div>
           )}
 
-          <div className='font-medium text-lg text-blue-600'>{property.name}</div>
-          <div className='font-normal text-sm text-gray-800'>{property.location.fullAddress}</div>
+          <div className='font-medium text-lg' style={{ color: '#0380EA' }}>
+            {property.name}
+          </div>
+          <div className='font-normal text-sm text-gray-800'>
+            {property.location.fullAddress}
+          </div>
         </div>
         <div className='flex flex-col items-start'>
           <div className='flex align-bottom mt-4 mb-4 font-normal text-sm text-gray-800'>
             {rentalType !== RentalType.Apartment && (
               <div className='flex mr-4'>
-                <Image
-                  src={Square}
-                  alt='square'
-                  className='mr-1'
-                />
+                <Image src={Square} alt='square' className='mr-1' />
                 <div>{property.totalArea} сот</div>
               </div>
             )}
 
             <div className='flex mr-4'>
-              <Image
-                src={Fridge}
-                alt='fridge'
-                className='mr-1'
-              />
+              <Image src={Fridge} alt='fridge' className='mr-1' />
               <div>
                 {property.kitchenArea}м<sup>2</sup>
               </div>
@@ -134,21 +134,13 @@ export default function Card({ property, rentalType }: CardProps) {
             {rentalType === RentalType.Apartment && (
               <>
                 <div className='flex mr-4'>
-                  <Image
-                    src={Bed}
-                    alt='bed'
-                    className='mr-1'
-                  />
+                  <Image src={Bed} alt='bed' className='mr-1' />
                   <div>
                     {property.livingArea}м<sup>2</sup>
                   </div>
                 </div>
                 <div className='flex mr-4'>
-                  <Image
-                    src={Tabler}
-                    alt='tabler'
-                    className='mr-1'
-                  />
+                  <Image src={Tabler} alt='tabler' className='mr-1' />
                   <div>
                     {property.floor}/{property.floors} этаж
                   </div>
